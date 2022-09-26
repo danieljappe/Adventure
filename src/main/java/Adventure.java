141d65838f4d56eddecc4a8735e7d785dd808777
YmerA38 <stin166c@stud.kea.dk>
2022-09-26 15:40:09 +0200

    tilføjet iml til gitignore

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -1,3 +1,12 @@
 public class Adventure {
+    Room room1 = new Room("","","","","","");
+    Room room2 = new Room("","","","","","");
+    Room room3 = new Room("","","","","","");
+    Room room4 = new Room("","","","","","");
+    Room room5 = new Room("","","","","","");
+    Room room6 = new Room("","","","","","");
+    Room room7 = new Room("","","","","","");
+    Room room8 = new Room("","","","","","");
+    Room room9 = new Room("","","","","","");
 
 }
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -5,4 +5,14 @@ public class Room {
     private String southLeadTo;
     private String eastLeadTo;
     private String westLeadTo;
+
+    public Room(String roomName,String roomDescription,
+                String northLeadTo, String southLeadTo, String eastLeadTo, String westLeadTo){
+        this.roomName = roomName;
+        this.roomDescription = roomDescription;
+        this.northLeadTo= northLeadTo;
+        this.southLeadTo = southLeadTo;
+        this.eastLeadTo= eastLeadTo;
+        this.westLeadTo=westLeadTo;
+    }
 }
