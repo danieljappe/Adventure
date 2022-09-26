56c664f56a6d375cc85704823d5730e1781c12b3
danieljappe <danieljappe@icloud.com>
2022-09-26 15:10:52 +0200

    3. oprettet switch case i UI

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class UserInterface {
     public void start() {
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.println("Type \"North\" / \"East\" / \"South\" / \"West\"");
+            String choice = scan.nextLine();
+
+            switch (choice) {
+                case "north" -> System.out.println("Going north");
+                case "east" -> System.out.println("Going east");
+                case "south" -> System.out.println("Going south");
+                case "west" -> System.out.println("Going west");
+                case "exit" -> System.exit(0);
+                default -> System.out.println("Invalid input");
+            }
+        } while (true);
     }
 }
+
