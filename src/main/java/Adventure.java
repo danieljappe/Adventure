630a5c63ba1cdcf0a2c060a54fb9039d3e0c6277
danieljappe <danieljappe@icloud.com>
2022-09-26 15:43:46 +0200

    opdateret adventure

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -1,12 +1,12 @@
 public class Adventure {
-    Room room1 = new Room("","","","","","");
-    Room room2 = new Room("","","","","","");
-    Room room3 = new Room("","","","","","");
-    Room room4 = new Room("","","","","","");
-    Room room5 = new Room("","","","","","");
-    Room room6 = new Room("","","","","","");
-    Room room7 = new Room("","","","","","");
-    Room room8 = new Room("","","","","","");
-    Room room9 = new Room("","","","","","");
+    Room room1 = new Room("room1","room1 description","","","","");
+    Room room2 = new Room("room2","room2 description","","","","");
+    Room room3 = new Room("room3","room3 description","","","","");
+    Room room4 = new Room("room4","room4 description","","","","");
+    Room room5 = new Room("room5","room5 description","","","","");
+    Room room6 = new Room("room6","room6 description","","","","");
+    Room room7 = new Room("room7","room7 description","","","","");
+    Room room8 = new Room("room8","room8 description","","","","");
+    Room room9 = new Room("room9","room9 description","","","","");
 
 }
