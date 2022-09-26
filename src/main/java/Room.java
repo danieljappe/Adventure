de60483582a6564aaee5ff913a8acc67a80fcf3a
YmerA38 <stin166c@stud.kea.dk>
2022-09-26 15:33:04 +0200

    tilføjet iml til gitignore

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,8 @@
 public class Room {
+    private String roomName;
+    private String roomDescription;
+    private String northLeadTo;
+    private String southLeadTo;
+    private String eastLeadTo;
+    private String westLeadTo;
 }
