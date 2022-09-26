7781a36571c737ffa08becd8f74fbfdab9096893
danieljappe <danieljappe@icloud.com>
2022-09-26 15:21:53 +0200

    tilføjet look, help og exit

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -5,14 +5,16 @@ public class UserInterface {
         Scanner scan = new Scanner(System.in);
 
         do {
-            System.out.println("Type \"North\" / \"East\" / \"South\" / \"West\"");
+            System.out.println("Type \"go north\" / \"go east\" / \"go south\" / \"go west\"");
             String choice = scan.nextLine();
 
             switch (choice) {
-                case "north" -> System.out.println("Going north");
-                case "east" -> System.out.println("Going east");
-                case "south" -> System.out.println("Going south");
-                case "west" -> System.out.println("Going west");
+                case "go north" -> System.out.println("Going north");
+                case "go east" -> System.out.println("Going east");
+                case "go south" -> System.out.println("Going south");
+                case "go west" -> System.out.println("Going west");
+                case "look" -> System.out.println("Kigger rundt i rum");
+                case "help" -> System.out.println("Print hjælp ud");
                 case "exit" -> System.exit(0);
                 default -> System.out.println("Invalid input");
             }
