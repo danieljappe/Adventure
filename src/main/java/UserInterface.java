fe646d353b4a1787e072ca8a57366f5e0891a7d5
danieljappe <danieljappe@icloud.com>
2022-09-26 15:27:20 +0200

    tilføjet flere søgningsmuligheder

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -5,14 +5,14 @@ public class UserInterface {
         Scanner scan = new Scanner(System.in);
 
         do {
-            System.out.println("Type \"go north\" / \"go east\" / \"go south\" / \"go west\"");
-            String choice = scan.nextLine();
+            System.out.println("Go north/west/east/south");
+            String choice = scan.nextLine().toLowerCase();
 
             switch (choice) {
-                case "go north" -> System.out.println("Going north");
-                case "go east" -> System.out.println("Going east");
-                case "go south" -> System.out.println("Going south");
-                case "go west" -> System.out.println("Going west");
+                case "go north","north","n" -> System.out.println("Going north");
+                case "go east","east","e" -> System.out.println("Going east");
+                case "go south","south","s" -> System.out.println("Going south");
+                case "go west","west","w" -> System.out.println("Going west");
                 case "look" -> System.out.println("Kigger rundt i rum");
                 case "help" -> System.out.println("Print hjælp ud");
                 case "exit" -> System.exit(0);
